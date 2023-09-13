78dacffcd4b9b9975774cf167bac68fe5fdb3c78
ffsdeji <obeayodeji@ymail.com>
2023-09-12 17:43:19 -0700

    testing

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#define main_h
+#ifndef main_h
 #define main_h
 int _putchar(char);
 void print_alphabet(void);
@@ -14,4 +14,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#end
+#endif
